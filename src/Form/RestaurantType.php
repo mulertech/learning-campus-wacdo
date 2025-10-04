1dfd8c84170c6a92576069d65c797b7872c3ac6f
Sébastien Muler <sebastien.muler@mulertech.net>
2025-10-04 14:35:11 +0200

    Enhance UI: update form styles for improved aesthetics; center headings and adjust button designs for consistency across pages.

--- src/Form/RestaurantType.php
+++ src/Form/RestaurantType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Restaurant;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,6 +17,7 @@ class RestaurantType extends AbstractType
             ->add('adresse')
             ->add('codePostal')
             ->add('ville')
+            ->add('submit', SubmitType::class, ['label' => 'Enregistrer'])
         ;
     }
 
